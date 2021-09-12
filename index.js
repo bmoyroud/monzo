bdd61aa191d231990de6c6c99bbb0233049c5550
Bastien <bastienm95@gmail.com>
2021-09-12 16:42:51 +0200

    Create Monzo class

--- index.js
+++ index.js
@@ -3,18 +3,30 @@ const axios = require("axios").default;
 const baseURL = "https://api.monzo.com";
 
 // TODO: move to .env
-const accessToken = "ACCESS_TOKEN";
+const accessToken =
+  "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJlYiI6IlBjNGV4ZGFnUWdPeWM2YTNKbjR0IiwianRpIjoiYWNjdG9rXzAwMDBBQkh0cVp0UG5FSVRCWHl4cmwiLCJ0eXAiOiJhdCIsInYiOiI2In0.ryWyNsAzG4-5VAHNBze3696wmab_a6ZJFptHaEIQ4e3-1gY6D_epfnLnS9lgkKlc0X0kUWrFhg2Jw5RHfRryfw";
 
-const instance = axios.create({
-  baseURL,
-  headers: {
-    Authorization: `Bearer ${accessToken}`,
-  },
-});
+class Monzo {
+  constructor(accessToken) {
+    this.client = axios.create({
+      baseURL,
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+  }
 
-// List accounts
-instance
-  .get("/accounts")
-  .then((response) => response.data)
-  .then((data) => console.log(data))
-  .catch(console.log);
+  get accounts() {
+    return {
+      list: () =>
+        this.client
+          .get("/accounts")
+          .then((response) => response.data)
+          .then((data) => console.log(data))
+          .catch(console.log),
+    };
+  }
+}
+
+const monzo = new Monzo(accessToken);
+monzo.accounts.list();
