2b2d2fb192fef07ad46569e7a089b35585c999d9
Bastien <bastienm95@gmail.com>
2021-09-12 17:33:36 +0200

    Add env variables

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
+require("dotenv").config();
 const axios = require("axios").default;
 
 const baseURL = "https://api.monzo.com";
-
-// TODO: move to .env
-const accessToken =
-  "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJlYiI6IlBjNGV4ZGFnUWdPeWM2YTNKbjR0IiwianRpIjoiYWNjdG9rXzAwMDBBQkh0cVp0UG5FSVRCWHl4cmwiLCJ0eXAiOiJhdCIsInYiOiI2In0.ryWyNsAzG4-5VAHNBze3696wmab_a6ZJFptHaEIQ4e3-1gY6D_epfnLnS9lgkKlc0X0kUWrFhg2Jw5RHfRryfw";
 
 class Monzo {
   constructor(accessToken) {
@@ -78,12 +75,13 @@ class Monzo {
 }
 
 (async () => {
+  const accessToken = process.env.ACCESS_TOKEN;
   const monzo = new Monzo(accessToken);
 
   const accounts = await monzo.accounts.list();
   console.log("Accounts", accounts);
 
-  const accountId = "acc_00009GO9ZevTx6Ky3l9r7p";
+  const accountId = process.env.ACCOUNT_ID;
 
   const balance = await monzo.balance(accountId);
   console.log("Balance", balance);
