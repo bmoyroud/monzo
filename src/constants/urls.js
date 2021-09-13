1e3ac2b42903573dc28931bc155b738a4193566b
Bastien <bastienm95@gmail.com>
2021-09-13 17:41:37 +0200

    Add URL constants

---
+++ src/constants/urls.js
@@ -0,0 +1,32 @@
+const BASE_URL = "https://api.monzo.com";
+
+const WHO_AM_I_URL = "/ping/whoami";
+
+const ACCOUNTS_URL = "/accounts";
+
+const BALANCE_URL = "/balance";
+
+const POTS_URL = "/pots";
+
+const TRANSACTIONS_URL = "/transactions";
+
+const FEED_URL = "/feed";
+
+const ATTACHMENT_URL = "/attachment";
+
+const RECEIPTS_URL = "/transaction-receipts";
+
+const WEBHOOKS_URL = "/webhooks";
+
+module.exports = {
+  BASE_URL,
+  WHO_AM_I_URL,
+  ACCOUNTS_URL,
+  BALANCE_URL,
+  POTS_URL,
+  TRANSACTIONS_URL,
+  FEED_URL,
+  ATTACHMENT_URL,
+  RECEIPTS_URL,
+  WEBHOOKS_URL,
+};
