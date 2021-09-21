8dc1d98ec1b0a9b5615b2920fc5a97323e0f9572
Bastien <bastienm95@gmail.com>
2021-09-21 17:14:26 +0200

    Update examples

--- index.js
+++ index.js
@@ -15,70 +15,80 @@
   const whoAmI = await monzo.whoAmI();
   console.log("Who am I", whoAmI);
 
-  const accounts = await monzo.accounts.list();
+  const accounts = await monzo.accounts.list({ account_type: "uk_prepaid" });
   console.log("Accounts", accounts);
 
-  const balance = await monzo.balance.retrieve(accountId);
+  const balance = await monzo.balance.retrieve({ account_id: accountId });
   console.log("Balance", balance);
 
-  const pots = await monzo.pots.list(accountId);
+  const pots = await monzo.pots.list({ current_account_id: accountId });
   console.log("Pots", pots);
 
-  const potAfterDeposit = await monzo.pots.deposit(
-    potId,
-    accountId,
-    1,
-    "some_unique_string1"
-  );
+  const potAfterDeposit = await monzo.pots.deposit({
+    pot_id: potId,
+    source_account_id: accountId,
+    amount: 1,
+    dedupe_id: "some_unique_string1",
+  });
   console.log("Pot after deposit", potAfterDeposit);
 
-  const potAfterWithdrawal = await monzo.pots.withdraw(
-    potId,
-    accountId,
-    2,
-    "some_unique_string1"
-  );
+  const potAfterWithdrawal = await monzo.pots.withdraw({
+    pot_id: potId,
+    destination_account_id: accountId,
+    amount: 2,
+    dedupe_id: "some_unique_string1",
+  });
   console.log("Pot after withdrawal", potAfterWithdrawal);
 
-  // const transactions = await monzo.transactions.list(
-  //   accountId,
-  //   "",
-  //   "2021-09-01T23:00:00Z"
-  // );
-  // console.log("Transactions", transactions);
+  const transactions = await monzo.transactions.list({
+    account_id: accountId,
+    since: "",
+    before: "2021-09-01T23:00:00Z",
+  });
+  console.log("Transactions", transactions);
 
-  const transaction = await monzo.transactions.retrieve(transactionId);
+  const transaction = await monzo.transactions.retrieve({
+    transaction_id: transactionId,
+  });
   console.log("Transaction", transaction);
 
-  const transactionExpanded = await monzo.transactions.retrieve(
-    transactionId,
-    true
-  );
+  const transactionExpanded = await monzo.transactions.retrieve({
+    transaction_id: transactionId,
+    expand_merchant: true,
+  });
   console.log("Transaction expanded", transactionExpanded);
 
-  const transactionAfterAnnotation = await monzo.transactions.annotate(
-    transactionId,
-    { hi: "test" }
-  );
+  const transactionAfterAnnotation = await monzo.transactions.annotate({
+    transaction_id: transactionId,
+    annotations: { hi: "test" },
+  });
   console.log("Transaction after annotation", transactionAfterAnnotation);
 
-  await monzo.feed.create(accountId, "basic", {
-    title: "feed item title",
-    image_url: imageURL,
+  await monzo.feed.create({
+    account_id: accountId,
+    type: "basic",
+    params: {
+      title: "feed item title",
+      image_url: imageURL,
+    },
   });
   console.log("Feed item successfully created.");
 
-  const urls = await monzo.attachment.upload("receipt.png", "image/png", 1000);
+  const urls = await monzo.attachment.upload({
+    file_name: "receipt.png",
+    file_type: "image/png",
+    content_length: 1000,
+  });
   console.log("Attachment URLs", urls);
 
-  const attachment = await monzo.attachment.register(
-    transactionId,
-    imageURL,
-    "image/png"
-  );
+  const attachment = await monzo.attachment.register({
+    external_id: transactionId,
+    file_url: imageURL,
+    file_type: "image/png",
+  });
   console.log("Attachment", attachment);
 
-  await monzo.attachment.deregister(attachment.id);
+  await monzo.attachment.deregister({ id: attachment.id });
   console.log("Attachment successfully deregistered.");
 
   await monzo.receipts.create({
@@ -98,24 +108,24 @@
 
   const receiptId = "some_id";
 
-  const receipt = await monzo.receipts.retrieve(receiptId).catch(console.log);
+  const receipt = await monzo.receipts.retrieve({ external_id: receiptId });
   console.log("Receipt", receipt);
 
-  await monzo.receipts.delete(receiptId).catch((err) => console.log(err));
+  await monzo.receipts.delete({ external_id: receiptId });
   console.log("Receipt successfully deleted.");
 
-  const webhook = await monzo.webhooks.create(
-    accountId,
-    "https://requestbin.com/r/enn8nks932hpg"
-  );
+  const webhook = await monzo.webhooks.create({
+    account_id: accountId,
+    url: "https://requestbin.com/r/enn8nks932hpg",
+  });
   console.log("Webhook", webhook);
 
-  const webhooks = await monzo.webhooks.list(accountId);
+  const webhooks = await monzo.webhooks.list({ account_id: accountId });
   console.log("Webhooks", webhooks);
 
   for (let i = 0; i < webhooks.length; i++) {
     const { id: webhookId } = webhooks[i];
-    await monzo.webhooks.delete(webhookId);
+    await monzo.webhooks.delete({ webhook_id: webhookId });
     console.log(`Webhook ${webhookId} successfully deleted.`);
   }
 })();
