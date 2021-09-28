0401eeb60a10c80f0d8e2f98d70c4c3a0036a8fd
Bastien <bastienm95@gmail.com>
2021-09-28 11:52:09 +0200

    Update AccountType struct

--- src/structs/accounts/AccountType.js
+++ src/structs/accounts/AccountType.js
@@ -1,8 +1,10 @@
 const { object, optional, enums } = require("superstruct");
 
-const AccountType = object({
-  // TODO: add list of valid account types to constants?
-  account_type: optional(enums(["uk_prepaid", "uk_retail", "uk_retail_joint"])),
-});
+const AccountType = optional(
+  object({
+    // TODO: add list of valid account types to constants?
+    account_type: enums(["uk_prepaid", "uk_retail", "uk_retail_joint"]),
+  })
+);
 
 module.exports = AccountType;
