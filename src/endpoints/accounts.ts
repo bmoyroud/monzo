335aee973157a2dae216607673e138c81ed9b3bd
Bastien <bastienm95@gmail.com>
2021-10-01 18:36:16 +0200

    Add default parameter

--- src/endpoints/accounts.ts
+++ src/endpoints/accounts.ts
@@ -6,13 +6,17 @@ import { buildAccountsUrl } from "../utils/urls";
 
 export default (client: AxiosInstance) => {
   return {
-    list: (args?: Infer<typeof List>) => {
+    list: (args: Infer<typeof List> = {}) => {
       assert(args, List);
+
+      const { account_type, ...pagination } = args;
 
       const endpointUrl = buildAccountsUrl();
 
       return client
-        .get<void, { accounts: Account[] }>(endpointUrl, { params: args })
+        .get<void, { accounts: Account[] }>(endpointUrl, {
+          params: { account_type },
+        })
         .then((data) => data.accounts);
     },
   };
