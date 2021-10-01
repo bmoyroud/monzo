880ed241de0b47793ccac179f18ef718f50ec2f7
Bastien <bastienm95@gmail.com>
2021-10-02 00:16:24 +0200

    Add examples for /balance

---
+++ examples/balance.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const Monzo = require("../dist").default;
+
+(async () => {
+  const accessToken = process.env.ACCESS_TOKEN;
+  const accountId = process.env.ACCOUNT_ID;
+
+  try {
+    const monzo = new Monzo(accessToken);
+
+    // balance info for a specific account
+    const balanceInfo = await monzo.balance.retrieve({ account_id: accountId });
+    console.log("Balance info:", balanceInfo);
+
+    // amount spent today
+    const spentToday = balanceInfo.spend_today;
+    console.log("Spent today:", spentToday);
+  } catch (err) {
+    console.log("Error", err);
+  }
+})();
