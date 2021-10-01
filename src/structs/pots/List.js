61a1ff25f1872fc9f022ddefb2abbe24529f75b6
Bastien <bastienm95@gmail.com>
2021-10-01 17:13:30 +0200

    Rename current_account_id to account_id

--- src/structs/pots/List.js
+++ src/structs/pots/List.js
@@ -1,9 +1,3 @@
-const { object, string, optional } = require("superstruct");
-const Pagination = require("../common/Pagination");
+const AccountIdWithPagination = require("../common/AccountIdWithPagination");
 
-const List = object({
-  current_account_id: string(),
-  pagination: optional(Pagination),
-});
-
-module.exports = List;
+module.exports = AccountIdWithPagination;
