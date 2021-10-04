eb6894f02ad90f0e3468ea948612e3cd5995c9b0
Bastien <bastienm95@gmail.com>
2021-10-05 00:35:43 +0200

    Fix import

--- src/utils/pagination.ts
+++ src/utils/pagination.ts
@@ -1,5 +1,5 @@
 import { Infer } from "superstruct";
-import { Pagination } from "../structs/common/Pagination";
+import { Pagination } from "../structs/common/pagination";
 
 // ensure object has .created property
 // TODO: move to monzo.ts?
