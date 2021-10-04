e6e5fc5a1dfd2f6e8cdbe648c122acde7e2a9dc8
Bastien <bastienm95@gmail.com>
2021-10-04 18:37:11 +0200

    Fix hint not showing up on VSCode

--- src/structs/accounts/List.js
+++ src/structs/accounts/List.js
@@ -1,9 +1,11 @@
 const { object, optional, enums, assign } = require("superstruct");
 const { Pagination } = require("../common/Pagination");
 
+// TODO: add list of valid account types to constants?
+const Type = enums(["uk_prepaid", "uk_retail", "uk_retail_joint"]);
+
 const AccountType = object({
-  // TODO: add list of valid account types to constants?
-  account_type: optional(enums(["uk_prepaid", "uk_retail", "uk_retail_joint"])),
+  account_type: optional(Type),
 });
 
 const List = optional(assign(AccountType, Pagination));
