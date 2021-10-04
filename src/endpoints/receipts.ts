35238c5f39f7cd9818e2be5efc13e0743a447879
Bastien <bastienm95@gmail.com>
2021-10-04 20:32:07 +0200

    Add util to check receipt amounts

--- src/endpoints/receipts.ts
+++ src/endpoints/receipts.ts
@@ -1,16 +1,18 @@
-import { assert, Infer } from "superstruct";
+import { assert, create, Infer } from "superstruct";
 import Endpoint from "./endpoint";
 import ReceiptParams from "../structs/receipts/Receipt";
 import Id from "../structs/common/Id";
 import { Receipt } from "../monzo";
 import { buildReceiptsUrl } from "../utils/urls";
+import checkReceipt from "../utils/receipt";
 
 const endpointUrl = buildReceiptsUrl();
 
 class ReceiptsEndpoint extends Endpoint {
   save(args: Infer<typeof ReceiptParams>) {
-    assert(args, ReceiptParams);
-    return this.client.put<void, {}>(endpointUrl, args);
+    const receipt = create(args, ReceiptParams);
+    checkReceipt(receipt);
+    return this.client.put<void, {}>(endpointUrl, receipt);
   }
 
   retrieve(externalId: Infer<typeof Id>) {
@@ -21,6 +23,9 @@ class ReceiptsEndpoint extends Endpoint {
       .then((data) => data.receipt);
   }
 
+  /**
+   * Note: currently not working (403 - insufficient permissions).
+   */
   delete(externalId: Infer<typeof Id>) {
     assert(externalId, Id);
     const args = { external_id: externalId };
---
+++ src/utils/receipt.ts
@@ -0,0 +1,45 @@
+// TODO: remove any types
+// TODO: merge monzo type and superstruct receipt structs?
+
+import { MainItem, Receipt } from "../monzo";
+
+const calculateReducer = (acc: number, item: any) =>
+  acc + item.amount * item.quantity;
+
+// from docs, https://docs.monzo.com/#receipt-items
+
+// The amounts of the sub-items should add up to amount on the item.
+function checkSubItems(items: any[]) {
+  for (const item of items) {
+    const { amount, sub_items } = item;
+    if (sub_items) {
+      const subItemsAmount = sub_items.reduce(calculateReducer, 0);
+      console.log("Amounts", subItemsAmount);
+
+      if (subItemsAmount !== amount) {
+        throw new Error(
+          "The amounts of the sub-items should add up to amount on the item."
+        );
+      }
+    }
+  }
+}
+
+// All of the items together, plus the taxes, should add up to the Receipt total.
+// Note: this not what EAT does. They do not include taxes in calculation.
+function checkTotal(receipt: any) {
+  const { total: receiptTotal } = receipt;
+
+  const total = receipt.items.reduce(calculateReducer, 0);
+
+  if (total !== receiptTotal) {
+    throw new Error(
+      "All of the items together, plus the taxes, should add up to the Receipt total."
+    );
+  }
+}
+
+export default function checkReceipt(receipt: any) {
+  checkTotal(receipt);
+  checkSubItems(receipt.items);
+}
