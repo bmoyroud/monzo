0d10817bd23c56603fb326ba423189ac13c585fb
Bastien <bastienm95@gmail.com>
2021-10-05 21:38:48 +0200

    Use literal struct and union type to guide Typescript

--- src/structs/receipt/payment.ts
+++ src/structs/receipt/payment.ts
@@ -3,6 +3,7 @@ import {
   dynamic,
   enums,
   Infer,
+  literal,
   number,
   object,
   optional,
@@ -13,30 +14,46 @@ import { Currency } from "../common";
 
 // TODO: move valid payment types to constants?
 export type PaymentType = Infer<typeof PaymentType>;
-const PaymentType = enums(["card", "cash", "gift_card"]);
+const PaymentType = enums(["cash", "card", "gift_card"]);
 
 const LastFour = size(string(), 4);
 
+type BasePayment = Infer<typeof BasePayment>;
 const BasePayment = object({
   type: PaymentType,
   amount: number(),
   currency: Currency,
 });
 
+type CashPayment = Infer<typeof CashPayment>;
+const CashPayment = assign(
+  BasePayment,
+  object({
+    type: literal("cash"),
+  })
+);
+
+type CardPayment = Infer<typeof CardPayment>;
 const CardPayment = assign(
   BasePayment,
   object({
+    type: literal("card"),
     last_four: optional(LastFour),
   })
 );
 
+type GiftCardPayment = Infer<typeof GiftCardPayment>;
 const GiftCardPayment = assign(
   BasePayment,
   object({
+    type: literal("gift_card"),
     gift_card_type: optional(string()),
   })
 );
 
+// help TypeScript figure out the type
+export type Payment = CashPayment | CardPayment | GiftCardPayment;
+// @ts-ignore
 export const Payment = dynamic((val, _) => {
   // @ts-ignore
   switch (val.type) {
@@ -45,7 +62,8 @@ export const Payment = dynamic((val, _) => {
     case "gift_card":
       return GiftCardPayment;
     case "cash":
+      return CashPayment;
     default:
-      return BasePayment;
+      throw new Error("Only basic type allowed for feed item.");
   }
 });
