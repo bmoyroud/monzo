8716c9b2746b29dccb4ca11e904441bef1efdd4e
Bastien <bastienm95@gmail.com>
2021-10-05 15:00:21 +0200

    Add as const to narrow scope

--- src/constants/types.ts
+++ src/constants/types.ts
@@ -1,6 +1,6 @@
-export const accounts = ["uk_prepaid", "uk_retail", "uk_retail_joint"];
+export const accounts = ["uk_prepaid", "uk_retail", "uk_retail_joint"] as const;
 
-export const feedItems = ["basic"];
+export const feedItems = ["basic"] as const;
 
 // TODO: add all currencies?
-export const currencies = ["GBP", "USD"];
+export const currencies = ["GBP", "USD"] as const;
