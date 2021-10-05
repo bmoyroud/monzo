3275b8c0d31980f5793fedb96dacdf81fdc293f4
Bastien <bastienm95@gmail.com>
2021-10-05 21:28:20 +0200

    Improve Payment struct

--- src/structs/receipt/payment.ts
+++ src/structs/receipt/payment.ts
@@ -1,13 +1,51 @@
-import { enums, number, object, optional, string } from "superstruct";
+import {
+  assign,
+  dynamic,
+  enums,
+  Infer,
+  number,
+  object,
+  optional,
+  size,
+  string,
+} from "superstruct";
 import { Currency } from "../common";
 
 // TODO: move valid payment types to constants?
-const PaymentType = enums(["cash", "cash", "gift_card"]);
+export type PaymentType = Infer<typeof PaymentType>;
+const PaymentType = enums(["card", "cash", "gift_card"]);
 
-export const Payment = object({
+const LastFour = size(string(), 4);
+
+const BasePayment = object({
   type: PaymentType,
   amount: number(),
   currency: Currency,
-  last_four: optional(string()),
-  gift_card_type: optional(string()),
+});
+
+const CardPayment = assign(
+  BasePayment,
+  object({
+    last_four: optional(LastFour),
+  })
+);
+
+const GiftCardPayment = assign(
+  BasePayment,
+  object({
+    gift_card_type: optional(string()),
+  })
+);
+
+export const Payment = dynamic((val, _) => {
+  // @ts-ignore
+  switch (val.type) {
+    case "card":
+      return CardPayment;
+    case "gift_card":
+      return GiftCardPayment;
+    case "cash":
+    default:
+      return BasePayment;
+  }
 });
