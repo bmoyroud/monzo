cf9f53bcdfcdefdaa8f31f01159f10910b7c9c8f
Bastien <bastienm95@gmail.com>
2021-10-06 01:56:28 +0200

    Add types to checkReceipt

--- src/utils/receipt.ts
+++ src/utils/receipt.ts
@@ -1,15 +1,13 @@
-// TODO: remove any types
-// TODO: merge monzo type and superstruct receipt structs?
+import { ReceiptReq } from "../structs/receipt";
+import { BaseItem, Items } from "../structs/receipt/items";
 
-import { MainItem, Receipt } from "../types/monzo";
-
-const calculateReducer = (acc: number, item: any) =>
+const calculateReducer = (acc: number, item: BaseItem) =>
   acc + item.amount * item.quantity;
 
 // from docs, https://docs.monzo.com/#receipt-items
 
 // The amounts of the sub-items should add up to amount on the item.
-function checkSubItems(items: any[]) {
+function checkSubItems(items: Items) {
   for (const item of items) {
     const { amount, sub_items } = item;
     if (sub_items) {
@@ -27,10 +25,11 @@ function checkSubItems(items: any[]) {
 
 // All of the items together, plus the taxes, should add up to the Receipt total.
 // Note: this not what EAT does. They do not include taxes in calculation.
-function checkTotal(receipt: any) {
+function checkTotal(receipt: ReceiptReq) {
   const { total: receiptTotal } = receipt;
 
   const total = receipt.items.reduce(calculateReducer, 0);
+  console.log(receiptTotal, total);
 
   if (total !== receiptTotal) {
     throw new Error(
