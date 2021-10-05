62b05e0f63aad9c60af97c4a6dc24a12c0d35138
Bastien <bastienm95@gmail.com>
2021-10-05 15:20:24 +0200

    Reorder alphabetically

--- src/structs/common/refinements.ts
+++ src/structs/common/refinements.ts
@@ -1,6 +1,16 @@
 import { min, number, pattern, refine, string } from "superstruct";
 import { URL as NodeURL } from "url";
 
+export const HexColor = pattern(string(), /^#(?:[0-9a-fA-F]{3}){1,2}$/);
+
+export const Positive = min(number(), 0, { exclusive: true });
+
+// see https://stackoverflow.com/a/24544212/4658957
+export const RFC3339 = pattern(
+  string(),
+  /^\\d{4}-\\d{2}-\\d{2}T\\d{2}%3A\\d{2}%3A\\d{2}(?:%2E\\d+)?[A-Z]?(?:[+.-](?:08%3A\\d{2}|\\d{2}[A-Z]))?$/
+);
+
 export const URL = refine(string(), "URL", (value) => {
   try {
     new NodeURL(value);
@@ -9,13 +19,3 @@ export const URL = refine(string(), "URL", (value) => {
     return false;
   }
 });
-
-export const HexColor = pattern(string(), /^#(?:[0-9a-fA-F]{3}){1,2}$/);
-
-// see https://stackoverflow.com/a/24544212/4658957
-export const RFC3339 = pattern(
-  string(),
-  /^\\d{4}-\\d{2}-\\d{2}T\\d{2}%3A\\d{2}%3A\\d{2}(?:%2E\\d+)?[A-Z]?(?:[+.-](?:08%3A\\d{2}|\\d{2}[A-Z]))?$/
-);
-
-export const Positive = min(number(), 0, { exclusive: true });
