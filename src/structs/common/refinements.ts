3120cef3af96b58179081d2b53ee4c2da9c7307a
Bastien <bastienm95@gmail.com>
2021-10-06 02:57:41 +0200

    Fix RFC3339 regex

--- src/structs/common/refinements.ts
+++ src/structs/common/refinements.ts
@@ -8,7 +8,7 @@ export const Positive = min(number(), 0, { exclusive: true });
 // see https://stackoverflow.com/a/24544212/4658957
 export const RFC3339 = pattern(
   string(),
-  /^\\d{4}-\\d{2}-\\d{2}T\\d{2}%3A\\d{2}%3A\\d{2}(?:%2E\\d+)?[A-Z]?(?:[+.-](?:08%3A\\d{2}|\\d{2}[A-Z]))?$/
+  /^([0-9]+)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])[Tt]([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9]|60)(\.[0-9]+)?(([Zz])|([\+|\-]([01][0-9]|2[0-3]):[0-5][0-9]))$/
 );
 
 export const URL = refine(string(), "URL", (value) => {
