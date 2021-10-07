a5dabf229105365abbbf8fdf9427d5c13292640e
Bastien <bastienm95@gmail.com>
2021-10-07 17:12:30 +0200

    Add basic id generation

--- src/utils.ts
+++ src/utils.ts
@@ -20,6 +20,10 @@ abstract class Utils {
     const formatted = formatter.format(amount / 100);
     return formatted;
   }
+
+  static id() {
+    return "_" + Math.random().toString(36).substr(2, 9);
+  }
 }
 
 export default Utils;
